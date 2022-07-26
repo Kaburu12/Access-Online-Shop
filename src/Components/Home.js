1434b69c1c16c527f54648e56606c7d4d3864ef9
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-26 13:35:00 +0300

    style the navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return ( 
 		<div className="home">
-			<h1>Home!</h1>
+		
+			
 		</div>
+	
+		
 	 );
 }
  
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,8 +4,20 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
 
 	return (  
-		<div className="navbar">
- <NavLink
+		<div id="logo" className="logo nav-common" > <nav id="nav">
+			<ul className="nav-common">
+			<li><NavLink
+        to="#"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Access- <span> Shop</span>
+      </NavLink></li>
+   <li><NavLink
         to="/"
         /* set exact so it knows to only set activeStyle when route is deeply equal to link */
         exact
@@ -15,8 +27,9 @@ const Navbar = () => {
         }}
       >
         Home
-      </NavLink>
-      <NavLink
+				</NavLink></li>   
+				
+   <li> <NavLink
         to="/about"
         exact
         activeStyle={{
@@ -24,8 +37,8 @@ const Navbar = () => {
         }}
       >
         About
-      </NavLink>
-      <NavLink
+      </NavLink></li>  
+    <li> <NavLink
         to="/products"
         exact
         activeStyle={{
@@ -33,8 +46,8 @@ const Navbar = () => {
         }}
       >
         Products
-      </NavLink>
-	  <NavLink
+      </NavLink></li> 
+	 <li><NavLink
         to="/contact"
         exact
         activeStyle={{
@@ -42,17 +55,14 @@ const Navbar = () => {
         }}
       >
         Contact
-      </NavLink>
-	  <NavLink
-        to="#"
-        exact
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Log In
-      </NavLink>
+				</NavLink></li> 
+				
+			</ul>
+			<div className="cart nav-icon nav-common"><img src="https://res.cloudinary.com/de8cuyd0n/image/upload/v1520412544/E-commerce%20landing%20page/icons/shopping_cart_1x.png" alt="shopping-cart"/></div>
+			<div className="menu nav-icon nav-common"><img src="https://res.cloudinary.com/de8cuyd0n/image/upload/v1520412543/E-commerce%20landing%20page/icons/menu_1x.png" alt="hamburger-menu"/></div>
+		</nav>
 		</div>
+		
 	);
 }
  
