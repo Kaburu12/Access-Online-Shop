1953523cdf94d099e8a57e6dc8563e3f553aa7cf
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-27 18:32:02 +0300

    fetch order data to make a POST request

--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -5,7 +5,19 @@ const OrderForm = () => {
  //usestate return order item values that hold the state values to be posted
 	const [ identity, setIdentity ] = useState("");
 	const [location, setLocation] = useState("");
- const [ category, setCategory] = useState("");
+    const [ category, setCategory] = useState("");
+
+
+//posting order data using our fetch API
+fetch("https://server-app123.herokuapp.com/order", {
+	method: "POST",
+	headers:
+	  { "Content-Type": "application/json", },
+	body: JSON.stringify(orderItem)
+  })
+	.then((r) => r.json())
+	//new posted order
+	.then((newOrder) => (newOrder));
 
 
 	return ( 
