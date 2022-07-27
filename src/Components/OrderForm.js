97a1320d121ae851213a2b25e399c9d70022c842
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-27 18:41:58 +0300

    add onSubmit form function

--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -5,32 +5,32 @@ const OrderForm = () => {
  //usestate return order item values that hold the state values to be posted
 	const [ identity, setIdentity ] = useState("");
 	const [location, setLocation] = useState("");
-    const [ category, setCategory] = useState("");
+	const [ category, setCategory ] = useState("");
+	
+	function handleSubmit(event) {
+		//a constant that holds new order data to be posted
+		const orderItem = {
+			identity: identity,
+			location: location,
+			category: category,
+		};
 
-//a constant that holds new order data to be posted
-const orderItem = {
-	identity: identity,
-	location: location,
-	category: category,
-   
-};
+		//posting order data using our fetch API
+		fetch("https://server-app123.herokuapp.com/order", {
+			method: "POST",
+			headers:
+				{ "Content-Type": "application/json", },
+			body: JSON.stringify(orderItem)
+		})
+			.then((r) => r.json())
+			//new posted order
+			.then((newOrder) => (newOrder));
 
-//posting order data using our fetch API
-fetch("https://server-app123.herokuapp.com/order", {
-	method: "POST",
-	headers:
-	  { "Content-Type": "application/json", },
-	body: JSON.stringify(orderItem)
-  })
-	.then((r) => r.json())
-	//new posted order
-	.then((newOrder) => (newOrder));
-
-
+	}
 	return ( 
 		<div>
 		<div>Enter Your Correct Information To Order</div>
-		<form className="orderform" >
+		<form className="orderform" onSubmit={handleSubmit}>
 		<input
 		  type="text"
 		  placeholder="identity"
